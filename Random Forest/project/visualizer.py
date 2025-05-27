5092ebbf5b28ca7035389ff1d8015fce2573e5a7
AdityaJain-0 <144289519+AdityaJain-0@users.noreply.github.com>
2025-05-27 16:41:37 -0500

    Delete Random Forest directory

--- Random Forest/project/visualizer.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from main import features
-
-features.head(10).plot(kind='barh')
-plt.xlabel("Feature Importance")
-plt.title("Top 10 Important Features")
-plt.gca().invert_yaxis()
-plt.tight_layout()
-plt.show()
